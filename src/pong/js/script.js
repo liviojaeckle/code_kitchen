86c839933a9a9749411ceec9a9388558e827841e
Livio Jäckle <livio.jaeckle@students.fhnw.ch>
2024-02-29 13:02:59 +0100

    play arround with canvas

--- src/pong/js/script.js
+++ src/pong/js/script.js
@@ -2,4 +2,7 @@ const canvas = document.getElementById('pongGame');
 const ctx = canvas.getContext('2d');
 
 canvas.width = 800;
-canvas.height = 600;
+canvas.height = 200;
+
+ctx.fillStyle = 'red';
+ctx.fillRect(200, 100, 200, 200);
