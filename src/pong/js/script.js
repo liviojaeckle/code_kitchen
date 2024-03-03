88c3eedcafbcc52b8348bb03f349e432448770af
Livio Jäckle <livio.jaeckle@students.fhnw.ch>
2024-03-03 09:58:42 +0100

    create ball on canvas and play arround

--- src/pong/js/script.js
+++ src/pong/js/script.js
@@ -1,8 +1,24 @@
 const canvas = document.getElementById('pongGame');
 const ctx = canvas.getContext('2d');
 
-canvas.width = 800;
-canvas.height = 200;
+canvas.width =800;
+canvas.height = 600;
 
 ctx.fillStyle = 'red';
-ctx.fillRect(200, 100, 200, 200);
+ctx.fillRect(200, 200, 200, 200);
+
+let ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 25,
+};
+
+function showBall() {
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+    ctx.fillStyle = 'black';
+    ctx.fill();
+    ctx.closePath();
+}
+
+showBall();
