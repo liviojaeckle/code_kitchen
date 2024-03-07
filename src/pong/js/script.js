dc4ab1dcc2cdb4206d879a84c34b6844312e7d71
Livio Jäckle <livio.jaeckle@students.fhnw.ch>
2024-03-07 09:37:17 +0100

    the ball moves, but not the way i want it to

--- src/pong/js/script.js
+++ src/pong/js/script.js
@@ -12,6 +12,10 @@ const paddleHeight = 100;
 let paddle1Y = (canvas.height - paddleHeight) / 2;
 let paddle2Y = (canvas.height - paddleHeight) / 2;
 
+let ballSpeedX = 0.5;
+let ballSpeedY = 0.5;
+
+
 function showBall() {
     ctx.beginPath();
     ctx.arc(ballX, ballY, ballRadius, 0, Math.PI*2);
@@ -26,9 +30,17 @@ function drawPaddles() {
     ctx.fillRect(canvas.width - paddleWidth, paddle2Y, paddleWidth, paddleHeight);
 }
 
+function moveBall() {
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
+
+}
+
 function game() {
     showBall();
     drawPaddles();
+    moveBall();
+    requestAnimationFrame(game);
 }
 
 game();
