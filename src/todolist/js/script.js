d2951198ef10b25c262a30945b2c0a40f9f68ab5
Livio Jäckle <livio.jaeckle@students.fhnw.ch>
2024-05-06 12:04:52 +0200

    (1 hour of working) working on todo list

--- src/todolist/js/script.js
+++ src/todolist/js/script.js
@@ -5,7 +5,16 @@ document.getElementById('input').addEventListener("keypress", function(event) {
 });
 
 function addTask() {
-
+    const input = document.getElementById('input');
+    const newTask = input.value.trim();
+    if (newTask) {
+        const listItem = document.createElement('li');
+        listItem.textContent = newTask;
+        document.getElementById('todoList').appendChild(listItem);
+        input.value = '';
+    } else {
+        alert("Du musst eine Aufgabe eingeben.");
+    }
 }
 
 function deleteTask() {
